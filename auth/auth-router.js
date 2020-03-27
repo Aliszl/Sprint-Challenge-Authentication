e051273813294f619787479c375b0c5782e81092
Alison Law <alisparklaw@gmail.com>
2020-03-27 09:31:25 +0000

    structure and register post functional

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
+const helpers = require('../auth/users-helpers');
+const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  helpers.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+// router.post('/login', (req, res) => {
+//   // implement login
+// });
 
 module.exports = router;
